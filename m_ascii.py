2d2e5a96d53b8d38026acf0edf6f436d91ad00c9
yukinoxita <m1091057545@163.com>
2020-10-21 18:19:10 +0800

    little update

--- m_ascii.py
+++ m_ascii.py
@@ -2,7 +2,17 @@ def get_ascii(s,g):
     ans = ''
     bit = int(input('please input N : '))
     li = s.split(g)
+    if g == "null":
+        for i in range(0,len(s),2):
+            ans += chr(int(s[i:i+2],bit))
+        return ans
     for i in li:
         #print((chr(int(i, bit))), end='')
         ans += chr(int(i, bit))
     return ans
+
+if __name__ == '__main__':
+    s = input('str:')
+    g = input('split:')
+    ans = get_ascii(s,g)
+    print(ans)
